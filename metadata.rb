4867b4c2ea5d92907554a32e5f3cf7af645fd55f
Yet Another Clever Name <admin@yacn.pw>
2013-09-27 16:28:16 -0400

    initial berkshelf skeleton commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'chef-racket'
+maintainer       'Yet Another Clever Name'
+maintainer_email 'admin@yacn.pw'
+license          'All rights reserved'
+description      'Installs/Configures chef-racket'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
